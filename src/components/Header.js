767d671299e27f8ae4768fe7e4a4c6784f59aa2a
Kenneth Imade <kenneth.imade@yahoo.com>
2022-04-22 22:10:30 +0100

    designed header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 function Header() {
 
     return(
-        <header>
-            <nav>
-                {/* <Link to="/"/>
-                <Link />
-                <Link />
-                <Link /> */}
+        <header className="header">
+            <Link to="/" className="header--link header--title"><h1>Kenneth Imade</h1></Link>
+            <nav className="header--nav">
+                <Link className="nav--link header--link" to="/projects">Projects</Link>
+                <Link className="nav--link header--link" to="/blog">Blog</Link>
+                <Link className="nav--link header--link" to="/contact">Contact</Link>
             </nav>
         </header>
     )
