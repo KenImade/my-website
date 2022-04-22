31be36195d91f831b6cd393a0a14f2d52d7c9eab
Kenneth Imade <kenneth.imade@yahoo.com>
2022-04-22 20:46:07 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
+
+// how am i going to build this website???
+// 1. home page
+// 2. projects page
+// 3. blog page
+// 4. contact page
+// 5. coming soon page
 
 export default App;
