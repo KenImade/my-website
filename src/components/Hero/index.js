7e499022b9fd5fbe52a7ba832df50680b2211d42
Kenneth Imade <kenneth.imade@yahoo.com>
2022-05-01 20:51:55 +0100

    designed hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,22 @@
-import "./styles.css"
-import Header from "../../components/Header";
+import "./Hero.css"
+import heroImg from "./../../images/website-construction.gif";
+import Button from "../Button";
 
 
 function Hero() {
 
     return (
-        <>
-        </>
+        <section className="hero">
+            <div className="hero--text">
+                <h2 className="hero--text--welcome">Hi 👋, my name is</h2>
+                <h1 className="hero--text--name">Kenneth Imade.</h1>
+                <h2 className="hero--text--value">I build cool things on the web.</h2>
+            </div>
+            <img src={heroImg} alt="website under construction" className="hero--img"/>
+            <Button>
+                Get In Touch
+            </Button>
+        </section>
     )
 }
 
