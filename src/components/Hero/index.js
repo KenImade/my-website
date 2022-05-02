d66d3f4f69e87f2cdcf9659affd06dc5f251d3a7
Kenneth Imade <kenneth.imade@yahoo.com>
2022-05-02 13:45:27 +0100

    changed layout

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -7,15 +7,17 @@ function Hero() {
 
     return (
         <section className="hero">
-            <div className="hero--text">
-                <h2 className="hero--text--welcome">Hi 👋, my name is</h2>
-                <h1 className="hero--text--name">Kenneth Imade.</h1>
-                <h2 className="hero--text--value">I build cool things on the web.</h2>
-            </div>
+            <section className="left--hero--section">
+                <div className="hero--text">
+                    <h2 className="hero--text--welcome">Hi 👋, my name is</h2>
+                    <h1 className="hero--text--name">Kenneth Imade.</h1>
+                    <h2 className="hero--text--value">I build cool things on the web.</h2>
+                </div>
+                <Button>
+                    Get In Touch
+                </Button>
+            </section>
             <img src={heroImg} alt="website under construction" className="hero--img"/>
-            <Button>
-                Get In Touch
-            </Button>
         </section>
     )
 }
