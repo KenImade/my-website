726d0b795b0b92dcd1b01d608b4cc40e60247a4c
Kenneth Imade <kenneth.imade@yahoo.com>
2022-05-06 19:28:26 +0100

    created footer section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Jobs from './components/Jobs';
 import Navbar from './components/Navbar';
@@ -15,6 +16,7 @@ function App() {
       <Jobs />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,11 @@
-import { Link } from "react-router-dom";
-
+import "./Footer.css";
 
 
 function Footer() {
 
     return(
         <footer>
-            <section className="footer--links">
-                <div className="left--footer--links">
-                    <Link className="left--footer--link" to="/">Home</Link>
-                    <Link className="left--footer--link" to="/blog">Blog</Link>
-                    <Link className="left--footer--link" to="/contact">Contact</Link>
-                </div>
-
-                <div className="right--footer--links">
-                    <a className="right--footer--link" href="https://stackoverflow.com/users/18151855/kenneth-imade">Stackoverflow</a>
-                    <a className="right--footer--link" href="https://github.com/kenimade">Github</a>
-                </div>
-            </section>
-            <p className="footer--copyright">Copyright © Kenneth Imade 2022</p>
+            <p className="footer--text">Created by Kenneth Imade 2022</p>
         </footer>
     )
 }
