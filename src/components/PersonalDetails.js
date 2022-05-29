e2ca528bd749715afbfddb6ff76c184dafba0019
Kenneth Imade <kenneth.imade@yahoo.com>
2022-05-29 22:06:17 +0100

    created mobile responsiveness

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -1,13 +1,59 @@
-import "../styles/PersonalDetails.css";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import StyledSection from "./Utils/StyledSection";
+
+
+const PersonalDetailsSection = styled(StyledSection)`
+
+    .personal-details--title {
+        font-size: 2.5rem;
+        text-align: left;
+    }
+
+    .personal-details--text--div {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        padding: 0.5rem;
+        border-left: 3px solid var(--hover-color);
+        margin-top: 20px;
+        margin-bottom: 50px;
+    }
+
+    .personal-details--text {
+        font-size: 1.2rem;
+    }
+
+    .personal-details--link {
+        text-align: center;
+        color: var(--primary-color);
+        background-color: var(--hover-color);
+        font-size: large;
+        padding: 0.8rem;
+        border-radius: 10px;
+        width: auto;
+        transition: all 0.5s;
+    }
+
+    .personal-details--link:hover {
+        background-color: var(--headers-color);
+    }
+
+    @media(max-width: 780px) {
+        .personal-details--text {
+            font-size: 4vw;
+            text-align: left;
+            line-height: 18px;
+        }
+`
 
 function PersonalDetails() {
 
     return (
-        <section className="personal-details section margin">
+        <PersonalDetailsSection>
             <h2 className="personal-details--title">PERSONAL DETAILS</h2>
 
-            <div>
+            <div className="personal-details--text--div">
                 <p className="personal-details--text">
                     A few interesting things about me. I love to read science fiction (my favorite is Frank Herbert's Dune).
                     I am also an avid gamer. I love to play competitive strategy games and first-person shooters.
@@ -28,7 +74,7 @@ function PersonalDetails() {
             >
                 ABOUT ME
             </Link>
-        </section>
+        </PersonalDetailsSection>
     )
 }
 
