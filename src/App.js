64235445a593fcdd3ab114bdc88a08f4b6b50e5e
Kenneth Imade <kenneth.imade@yahoo.com>
2022-05-30 18:00:08 +0100

    modified routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import './styles/index.css';
-// import About from './components/About';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
 // import Jobs from './components/Jobs';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './Pages/Home';
 import Footer from './components/Footer';
+import Projects from './components/Projects';
+import Blog from './components/Blog';
+import About from "./Pages/About"
+import Posts from './components/Posts';
 // import Projects from './components/Projects';
 // import Blog from './components/Blog';
 // import TopNavigator from './components/Utils/TopNavigator';
 
 function App() {
+
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="blog" element={<Blog />}/>
+        <Route path="posts" element={<Posts />} />
+      </Routes>
       <Footer />
-      {/*<About />
-      <Jobs />
-      <Projects />
-      <Blog />
-      <Contact />
-      <Footer />
-      <TopNavigator /> */}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import App from './App';
-import Projects from './components/Projects';
-import Blog from './components/Blog';
-// import Contact from './components/Contact';
-import About from "./components/About"
-// import AllProjects from './components/AllProjects';
-import Posts from './components/Posts';
-// import BlogPost from './components/Utils/BlogPost';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/blog" element={<Blog />}/>
-          <Route path="/posts" element={<Posts />} />
-          {/* <Route path="/posts/:post_id" element={<BlogPost />} /> */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
-          {/* <Route path="/projects" element={<AllProjects />} /> */}
-        </Routes>
-      </BrowserRouter>
-    
+        <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
