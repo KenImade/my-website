fcbee6511303024f418b2ef5afea79bb01796935
Kenneth Imade <kenneth.imade@yahoo.com>
2022-06-01 14:59:13 +0100

    replaced text with icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 import StyledSection from "./Utils/StyledSection";
+import {DiPython, DiJava, DiLinux} from "react-icons/di";
+import {AiOutlineConsoleSql} from "react-icons/ai";
+import {SiJavascript} from "react-icons/si";
+
 
 const SkillSection = styled(StyledSection)`
     .skills--title {
@@ -10,9 +14,16 @@ const SkillSection = styled(StyledSection)`
     .skills--list {
         display: grid;
         justify-items: center;
-        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
         margin-top: 50px;
         gap: 1.2rem;
+    }
+
+    .skill-card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-conten: center;
     }
 
     ul {
@@ -29,15 +40,8 @@ const SkillSection = styled(StyledSection)`
     .skill-title {
         font-size: 1.1rem;
         text-align: center;
-        margin-bottom: 20px;
-        color: var(--header-color);
-    }
-
-    @media (max-width: 780px) {
-        .skills--list {
-            display: flex;
-            flex-direction: column;
-        }
+        color: var(--secondary-color);
+        margin-top: 10px;
     }
 `
 
@@ -48,31 +52,24 @@ function Skills () {
             <h2 className="skills--title">Skills Expertise</h2>
             <div className="skills--list">
                 <div className="skill-card">
-                    <h4 className="skill-title">Data Engineering</h4>
-                    <ul>
-                        <li>Data Extraction</li>
-                        <li>Data Cleaning</li>
-                        <li>Data Warehousing</li>
-                        <li>Python</li>
-                    </ul>
+                    <DiPython size="60px"/>
+                    <h4 className="skill-title">Python</h4>
                 </div>
                 <div className="skill-card">
-                    <h4 className="skill-title">Data Engineering</h4>
-                    <ul>
-                        <li>Data Extraction</li>
-                        <li>Data Cleaning</li>
-                        <li>Data Warehousing</li>
-                        <li>Python</li>
-                    </ul>
+                    <AiOutlineConsoleSql size="60px"/>
+                    <h4 className="skill-title">SQL</h4>
                 </div>
                 <div className="skill-card">
-                    <h4 className="skill-title">Data Engineering</h4>
-                    <ul>
-                        <li>Data Extraction</li>
-                        <li>Data Cleaning</li>
-                        <li>Data Warehousing</li>
-                        <li>Python</li>
-                    </ul>
+                    <DiJava size="60px"/>
+                    <h4 className="skill-title">Java</h4>
+                </div>
+                <div className="skill-card">
+                    <SiJavascript size="60px"/>
+                    <h4 className="skill-title">JavaScript</h4>
+                </div>
+                <div className="skill-card">
+                    <DiLinux size="60px"/>
+                    <h4 className="skill-title">Linux</h4>
                 </div>
             </div>
         </SkillSection>
