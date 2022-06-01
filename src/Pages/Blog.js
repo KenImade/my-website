693609da234e51cb0855e0efa417199d083bdca7
Kenneth Imade <kenneth.imade@yahoo.com>
2022-06-01 14:11:20 +0100

    setup and styled page

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,14 +1,73 @@
 import styled from "styled-components";
 import Banner from "../components/Utils/Banner";
+// import StyledSection from "../components/Utils/StyledSection";
 
 const BlogSection = styled.section`
-    ${'' /* background-color: #251D3A;
-    padding: 2.5em;
-    padding-top: 0.3em;
+    ${'' /* background-color: #251D3A; */}
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-items: center; */}
+    justify-items: center;
+    margin-bottom: 80px;
+    margin-top: 80px;
+    padding: 30px;
+
+    .post-section {
+        .post-section-title {
+            font-size: 1.8rem;
+        }
+    }
+
+    .post {
+        display: flex;
+        align-items: center;
+        gap: 1.1rem;
+        margin: 20px 0px;
+
+        .post-title {
+            font-size: 1.2rem;
+            color: var(--secondary-color);
+            transition: all 0.8s;
+        }
+
+        .post-title:hover {
+            color: var(--hover-color);
+            font-size: 1.4rem;
+        }
+        
+        sub {
+            color: var(--secondary-color);
+        }
+    }
+
+    @media(max-width: 780px) {
+        .post-section {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            .post-section-title {
+                text-align: center;
+            }
+            
+        }
+
+        .post {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            gap: 0;
+
+            a {
+                text-align: center;
+            }
+            h3 {
+                text-align: center;
+            }
+           
+        }
+    }
 `
 
 
@@ -18,7 +77,15 @@ function Blog(props) {
         <>
             <Banner text="Blog" />
             <BlogSection>
-            
+                <div class="post-section">
+                    <h2 className="post-section-title">Programming</h2>
+                    <div className="post">
+                        <a href="https://www.google.com">
+                            <h3 className="post-title">The Temporal Dead Zone in JavaScript</h3>
+                            <sub>1 June 2022</sub>
+                        </a>
+                    </div>
+                </div>
             </BlogSection>
         </>
     )
