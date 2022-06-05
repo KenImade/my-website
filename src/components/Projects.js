e8b41ce95b8aec9f5fa73fa8ec40083c0abea2eb
Kenneth Imade <kenneth.imade@yahoo.com>
2022-06-05 22:14:01 +0100

    added content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import StyledSection from "./Utils/StyledSection";
 import projImg1 from "./../images/website-snip.jpg";
 import cloudImg from "./../images/cloud.jpg";
+import omdenaImg from "./../images/omdena.png";
 import FadeIn from "./Utils/FadeIn";
 
 const ProjectSection = styled(StyledSection)`
@@ -44,7 +45,7 @@ const ProjectSection = styled(StyledSection)`
         font-size: 1.2rem;
         text-align: center;
         height: 40px;
-        margin: 10px 0px;
+        margin-top: 10px;
     }
 
     .project--card--title:hover {
@@ -84,17 +85,20 @@ function Projects() {
             <p className="projects--sub-title">Take a look at my recent projects.</p>
             <div className="projects--list">
                 <div className="project--card">
-                    <a target="_blank" rel="noreferrer" href="https://github.com/KenImade/predicting_the_price_of_used_cars_in_Nigeria">
+                    <a target="_blank" rel="noreferrer" href="https://medium.com/@kenimade91/how-to-deploy-a-static-website-to-the-cloud-using-amazon-s3-bucket-and-cloudfront-23db00572199">
                         <img src={cloudImg} alt="project-title"/>
                     </a>
                     <hr/>
                     <a className="project--card--title"
-                        href="https://github.com/KenImade/predicting_the_price_of_used_cars_in_Nigeria"
+                        href="https://medium.com/@kenimade91/how-to-deploy-a-static-website-to-the-cloud-using-amazon-s3-bucket-and-cloudfront-23db00572199"
+                        target="_blank"
+                        rel="noreferrer"
                     >
                         Deploying a static website to the cloud
                     </a>
                     <p className="project--card--desc">
-                        Utilized AWS resources, S3 bucket and Cloudfront to deploy a static website to the cloud.
+                        Utilizing AWS resources, S3 bucket and CloudFront. 
+                        I learnt how to deploy a static website to the cloud.
                     </p>
                 </div>
                 <div className="project--card">
@@ -104,25 +108,30 @@ function Projects() {
                     <hr/>
                     <a className="project--card--title"
                         href="https://github.com/KenImade/predicting_the_price_of_used_cars_in_Nigeria"
+                        target="_blank"
+                        rel="noreferrer"
                     >
                         Predicting the Price of Used Cars in Nigeria
                     </a>
                     <p className="project--card--desc">
-                        This aim of this project is to use data gotten from the Cars45 website to predict the price of a used car in Nigeria
+                        The aim of this project was to use data gotten from a popular nigerian car dealer website (Cars45) to predict the price of a used car in Nigeria
                     </p>
                 </div>
                 <div className="project--card">
-                    <a target="_blank" rel="noreferrer" href="https://github.com/KenImade/predicting_the_price_of_used_cars_in_Nigeria">
-                        <img src={projImg1} alt="project-title"/>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/OmdenaAI/omdena-nigeria-energy">
+                        <img src={omdenaImg} alt="project-title"/>
                     </a>
                     <hr/>
                     <a className="project--card--title"
-                        href="https://github.com/KenImade/predicting_the_price_of_used_cars_in_Nigeria"
+                        href="https://github.com/OmdenaAI/omdena-nigeria-energy"
+                        target="_blank"
+                        rel="noreferrer"
                     >
-                        Predicting the Price of Used Cars in Nigeria
+                        A.I for Energy: Transitioning Toward a Sustainable Energy System
                     </a>
                     <p className="project--card--desc">
-                        This aim of this project is to use data gotten from the Cars45 website to predict the price of a used car in Nigeria
+                        Open source project that utilized machine learning to identify areas with little or no electricity
+                        in Nigeria.
                     </p>
                 </div>
             </div>
